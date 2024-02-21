59197ec2f6c4576b32d05b28306585ca15876f36
Taku1103 <orzorz1243@gmail.com>
2024-02-22 04:27:32 +0900

    tk-22.netをconfig hostに加える

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "tk-22.net"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
